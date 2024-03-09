7758b3044f2e732ad179beeaa45535ae1911c193
chgiselle0210 <157499652+chgiselle0210@users.noreply.github.com>
2024-03-08 20:39:05 -0600

    Delete biotech/src directory

--- biotech/src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-// App.test.jsx
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-
-test('renders main component correctly', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.exists()).toBe(true);
-}); 
